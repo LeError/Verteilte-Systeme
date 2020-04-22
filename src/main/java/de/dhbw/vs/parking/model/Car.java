d39673682fbc760f7bfc2a74d8b873bb9344f49c
LeError <herder.robin@Icloud.com>
2020-04-22 13:15:09 +0200

    Added Base Implementation for Car Entity

---
+++ src/main/java/de/dhbw/vs/parking/model/Car.java
@@ -0,0 +1,48 @@
+package de.dhbw.vs.parking.model;
+
+import de.dhbw.vs.parking.exceptions.IllegalDataStateException;
+import de.dhbw.vs.parking.utli.PredicateUtils;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+
+@Entity
+@NoArgsConstructor
+public class Car {
+
+    @Id
+    @Column
+    private String licensePlate;
+
+    @OneToMany (cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private Set<Visit> visits = new HashSet<>();
+
+    public Set<Visit> getBilledVisits() {
+        return visits.stream().filter(Visit::isPayed).collect(Collectors.toSet());
+    }
+
+    public Set<Visit> getNotBilledVisits() {
+        return visits.stream().filter(PredicateUtils.not(Visit::isPayed)).filter(Visit::isVisitFinished).collect(Collectors.toSet());
+    }
+
+    public Set<Visit> getVisits() {
+        return visits.stream().filter(Visit::isVisitFinished).collect(Collectors.toSet());
+    }
+
+    public Visit getOnGoingVisit() throws IllegalDataStateException {
+        Set<Visit> visits = this.visits.stream().filter(Visit::isVisitFinished).collect(Collectors.toSet());
+        if(visits.size() > 2) {
+            throw new IllegalDataStateException("Two unfinished visits with one car are impossible. Illegal State!");
+        }
+        if(visits.isEmpty()) {
+            return null;
+        }
+        return visits.stream().findFirst().get();
+    }
+
+}
